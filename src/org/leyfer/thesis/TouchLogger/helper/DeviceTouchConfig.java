6056c3a6e4c233c226a1892f0a246611316a8ca3
Kirill <kolbasisha@gmail.com>
2016-01-09 21:48:43 +0600

    Added Note3 (no LTE) to config.

--- src/org/leyfer/thesis/TouchLogger/helper/DeviceTouchConfig.java
+++ src/org/leyfer/thesis/TouchLogger/helper/DeviceTouchConfig.java
@@ -17,16 +17,9 @@ public class DeviceTouchConfig {
             new Type("002f", "0035", "0036", "003a", "0039")
     );
 
-    // LG Nexus 5 (AOSP)
-    public static final TouchConfig hammerhead_AOSP_on_HammerHead_Config = new TouchConfig(
-            "/dev/input/event1",
-            new Code("0003", "0000"),
-            new Range(0, 9, 255),
-            new Type("002f", "0035", "0036", "003a", "0039")
-    );
 
     // Samsung galaxy note 3
-    public static final TouchConfig MSM8974_SM_N9005_Config = new TouchConfig(
+    public static final TouchConfig MSM8974_SM_N900_Config = new TouchConfig(
             "/dev/input/event1",
             new Code("0003", "0000"),
             new Range(0, 9, 255),
@@ -43,8 +36,9 @@ public class DeviceTouchConfig {
 
     public static final Map<String, TouchConfig> configMap = new HashMap<String, TouchConfig>() {{
         put("hammerhead,Nexus 5", hammerhead_Nexus_5_Config);
-        put("hammerhead,AOSP on HammerHead", hammerhead_AOSP_on_HammerHead_Config);
-        put("MSM8974,SM-N9005", MSM8974_SM_N9005_Config);
+        put("hammerhead,AOSP on HammerHead", hammerhead_Nexus_5_Config);
+        put("universal5420,SM-N900", MSM8974_SM_N900_Config);
+        put("MSM8974,SM-N9005", MSM8974_SM_N900_Config);
         put("MSM8974,LG-D802", MSM8974_LG_D802_Config);
     }};
 }
