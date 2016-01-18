0508d9ea6ccc50388d84dc98169b655b5b1dbff9
Kirill <kolbasisha@gmail.com>
2016-01-18 18:06:44 +0600

    Added support for HTC Desire C.

--- src/org/leyfer/thesis/TouchLogger/helper/DeviceTouchConfig.java
+++ src/org/leyfer/thesis/TouchLogger/helper/DeviceTouchConfig.java
@@ -65,6 +65,14 @@ public class DeviceTouchConfig {
             new Type("002f", "0035", "0036", "003a", "0039")
     );
 
+    // Htc desire C
+    public static final TouchConfig GOLFU_HTC_DESIRE_C_Config = new TouchConfig(
+            "/dev/input/event5",
+            new Code("0003", "0000"),
+            new Range(0, 3, 30),
+            new Type("002f", "0035", "0036", "0032", "0039")
+    );
+
     public static final Map<String, TouchConfig> configMap = new HashMap<String, TouchConfig>() {{
         put("HAMMERHEAD,NEXUS 5", hammerhead_Nexus_5_Config);
         put("HAMMERHEAD,NEXUS 5 CAF", hammerhead_Nexus_5_Config);
@@ -76,5 +84,6 @@ public class DeviceTouchConfig {
         put("GALBI,LG-D802", MSM8974_LG_D802_Config);
         put("CLOVERTRAIL,LENOVO K900_ROW", clovertrail_Lenovo_K900_ROW_Config);
         put("MSM8960,GT-I9190", MSM8960_GT_I9190_Config);
+        put("GOLFU,HTC DESIRE C", GOLFU_HTC_DESIRE_C_Config);
     }};
 }
